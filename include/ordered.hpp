966ad7f2a4f7b322734d25932b9533845ec891d0
Magnus Burenius <magnus.burenius@gmail.com>
2022-02-22 22:59:30 +0100

    add algorithm mid_element with compare function

--- include/ordered.hpp
+++ include/ordered.hpp
@@ -11,10 +11,27 @@ RandomAccessIterator mid_element(RandomAccessIterator first, RandomAccessIterato
     return mid;
 }
 
+template<typename RandomAccessIterator, typename Compare>
+RandomAccessIterator mid_element(RandomAccessIterator first, RandomAccessIterator last, Compare comp)
+{
+    const auto num_elements = std::distance(first, last);
+    const auto mid = first + num_elements / 2;
+    std::nth_element(first, mid, last, comp);
+    return mid;
+}
+
 template<typename Container>
 typename Container::iterator mid_element(Container& container)
 {
     using std::begin;
     using std::end;
     return mid_element(begin(container), end(container));
+}
+
+template<typename Container, typename Compare>
+typename Container::iterator mid_element(Container& container, Compare comp)
+{
+    using std::begin;
+    using std::end;
+    return mid_element(begin(container), end(container), comp);
 }
