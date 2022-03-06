d7596ede33baf4787c92e721609b4c4e9d001a01
Magnus Burenius <magnus.burenius@gmail.com>
2022-03-06 13:43:28 +0100

    refactor: extract min_element.hpp

--- include/std_algorithms_container.hpp
+++ include/std_algorithms_container.hpp
@@ -4,6 +4,7 @@
 #include <cassert>
 
 #include "traits.hpp"
+#include "min_element.hpp"
 
 namespace aaa {
 
@@ -35,72 +36,6 @@ void fill(Container& container, const Value& value)
     using std::begin;
     using std::end;
     std::fill(begin(container), end(container), value);
-}
-
-template<typename Container>
-typename Container::iterator min_element(Container& container)
-{
-    using std::begin;
-    using std::end;
-    return std::min_element(begin(container), end(container));
-}
-
-template<typename Container>
-typename Container::const_iterator min_element(const Container& container)
-{
-    using std::begin;
-    using std::end;
-    return std::min_element(begin(container), end(container));
-}
-
-template<typename Container, typename Compare, check_compare<Compare, value_type<Container>> = nullptr>
-typename Container::iterator min_element(Container& container, Compare comp)
-{
-    using std::begin;
-    using std::end;
-    return std::min_element(begin(container), end(container), comp);
-}
-
-template<typename Container, typename Compare, check_compare<Compare, value_type<Container>> = nullptr>
-typename Container::const_iterator min_element(const Container& container, Compare comp)
-{
-    using std::begin;
-    using std::end;
-    return std::min_element(begin(container), end(container), comp);
-}
-
-template<typename Iterator, typename Key, check_key<Key, value_type_i<Iterator>> = nullptr>
-Iterator min_element(Iterator first, Iterator last, Key key)
-{
-    if (first == last) {
-        return first;
-    }
-    auto result = first;
-    auto value = key(*first);
-    while (++first != last) {
-        auto new_value = key(*first);
-        if (new_value < value) {
-            result = first;
-            value = new_value;
-        }
-    }
-    return result;
-}
-
-template<typename Container, typename Key, check_key<Key, value_type<Container>> = nullptr>
-typename Container::iterator min_element(Container& container, Key key)
-{
-    using std::begin;
-    using std::end;
-    return min_element(begin(container), end(container), key);
-}
-
-template<typename Container, typename Key, check_key<Key, value_type<Container>> = nullptr>
-typename Container::const_iterator min_element(const Container& container, Key key)
-{
-    using std::begin;
-    using std::end;
-    return aaa::min_element(begin(container), end(container), key);
 }
 
 template<typename Container>
